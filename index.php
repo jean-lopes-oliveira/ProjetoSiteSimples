7309cedf2915adfbd925897d0f95de206b983fce
jean-lopes-oliveira <70926946+jean-lopes-oliveira@users.noreply.github.com>
2023-06-26 01:34:47 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-    <meta charset="UTF-12">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="./css/style.css">
@@ -30,39 +30,19 @@
     </header>
     <main id="main" class="container-fluid">
         <article id="servicos" class="row  justify-content-center align-items-center">
-            <div class="conteudo col-xs-12 col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+            <div class="conteudo col-xs-12 col-12 col-sm-8 col-md-8 col-lg-8 col-xl-8">
             <header>
                 <h1>Serviços</h1>
             </header>
             <section>
                <p> Projetos de implantação de usinas de energia solar.</p>
             </section>
-            <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="p.jpg" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="telefone.jpg" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="email.jpg" class="d-block w-100" alt="...">
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Anterior</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Próximo</span>
-  </button>
-</div>
+            
             </div>
         </article>
         
         <article id="missao" class="row justify-content-center align-items-center">
-        <div class="conteudo col-xs-12 col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+        <div class="conteudo col-xs-12 col-12 col-sm-8 col-md-8 col-lg-8 col-xl-8">
             <header>
                 <h1>Missão</h1>
             </header>
@@ -72,7 +52,7 @@
         </div>
         </article>
         <article id="visao" class="row justify-content-center align-items-center">
-        <div class="conteudo col-xs-12 col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+        <div class="conteudo col-xs-12 col-12 col-sm-8 col-md-8 col-lg-8 col-xl-8">
             <header>
                 <h1>Visão</h1>
             </header>
@@ -82,7 +62,7 @@
         </div>
         </article>
         <article id="valores" class="row justify-content-center align-items-center">
-        <div class="conteudo col-xs-12 col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 ">
+        <div class="conteudo col-xs-12 col-12 col-sm-8 col-md-8 col-lg-8 col-xl-8 ">
             <header>
                 <h1>Valores</h1>
             </header>
@@ -92,7 +72,7 @@
         </div>
         </article>
         <article id="contato" class="row justify-content-center align-items-center">
-        <div class="conteudo col-xs-12 col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+        <div class="conteudo col-xs-12 col-12 col-sm-8 col-md-8 col-lg-8 col-xl-8">
             <form id="form-contato">
                     <h1>Contato</h1>
                     <label for="nome">Nome</label><input type="text" name="nome" id="nome" required class="form-control">
